c615da0163cc4188f748faecb4f343694f2b2418
AOS2019 <samuel90ezekiel@gmail.com>
2025-03-28 02:10:11 +0100

    convert the entire layout to a Client Component by adding 'use client'; at the top

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 // File: app/layout.tsx
+'use client';
+
 import './globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en"
+    suppressHydrationWarning="true"
+    data-qb-installed="true">
       <body>
         <QueryClientProvider client={queryClient}>
           {children}
