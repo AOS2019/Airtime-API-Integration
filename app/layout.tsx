3d0a35ae9bd95aff59ec590f9223c76b64bfc0c0
AOS2019 <samuel90ezekiel@gmail.com>
2025-03-28 02:38:06 +0100

    added suppressHydrationWarning to the html element

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,25 @@
-// File: app/layout.tsx
 'use client';
 
-import './globals.css';
+import { useEffect, useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import './globals.css';
 
 const queryClient = new QueryClient();
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  // Set isMounted to true only on the client
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
   return (
-    <html lang="en"
-    suppressHydrationWarning="true"
-    data-qb-installed="true">
+    <html
+      lang="en"
+      suppressHydrationWarning
+      {...(isMounted ? { 'data-qb-installed': 'true' } : {})}
+    >
       <body>
         <QueryClientProvider client={queryClient}>
           {children}
